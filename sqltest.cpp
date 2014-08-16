e270fb66903b7888fcdf7b76fb50e0013e5c7a4a
Genival Veloso de França Neto <netoovf@live.com>
2014-08-16 16:57:54 -0300

    Create sqltest.cpp

---
+++ sqltest.cpp
@@ -0,0 +1,18 @@
+#include "sqltest.h"
+#include "ui_sqltest.h"
+
+#include <QMessageBox>
+#include <QtGui>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+
+
+sqltest::sqltest(QWidget *parent):
+    QDialog(parent),
+    ui(new Ui::sqltest)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("SQL TESTE");
+    habilitarQuer(false);
+}
